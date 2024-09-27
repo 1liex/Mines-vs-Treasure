53edfe522f109813cf332f25488873047bf4cc1b
Dexter <80363905+Dexy-69@users.noreply.github.com>
2024-09-27 21:27:27 +0300

    تغيرات كثيره
    غيرت الفانكشن تبع ريتيرن وخليتها من نفس game.html
    سويت الفانكشن تبع الاسم ويتنقل للصفحه الثانيه
    رتبت شويا شكل الاكواد في الجافا
    وفيه شرح كمان

--- game.js
+++ game.js
@@ -1,25 +1,29 @@
 function StatrGameEasy() 
 {
-	window.location.href = 'Game.html';
-	let Name = document.getElementById("input-the-name").value
-	let showName = document.getElementById("show-name")
-	
-}
+    let Name = document.getElementById("input-the-name").value;
+    
+    // تحقق إذا كان الاسم موجود
+    if (!Name) {
+        alert("HELLO ! \n Enter your name first"); // رسالة خطأ
+        return;
+    }
 
+    localStorage.setItem("playerName", Name); // تخزين الاسم
+    window.location.href = 'Game.html'; // الانتقال إلى الصفحة الثانية
+}
+//----------------------------------------------------------------------------------------------------------
 function StartGameHard() 
 {
-	window.location.href = 'Game.html';
-	game()
-	
+    let Name = document.getElementById("input-the-name").value;
+    if (!Name) 
+	{
+        alert("HELLO ! \n Enter your name first"); // رساله خطأ
+        return;
+    }
+    localStorage.setItem("playerName", Name); // تخزين الاسم
+    window.location.href = 'Game.html'; // الانتقال إلى الصفحة الثانية
 }
-
-function Return()
-{
-	window.location.href = 'Home.html';
-}
-
-document.addEventListener("DOMContentLoaded", game() )
-
+//----------------------------------------------------------------------------------------------------------
 function game() 
 {
 	let canvas = document.getElementById('canvas');
@@ -28,16 +32,29 @@ function game()
 	let cols = 8;
 	let cellSize = canvas.width / cols;
 	// رسم المصفوفة
-	for (let row = 0; row < rows; row++) 
-	{
-		for (let col = 0; col < cols; col++)
-		{
+	for (let row = 0; row < rows; row++) {
+		for (let col = 0; col < cols; col++) {
 			// حساب إحداثيات المربع
 			let x = col * cellSize;
 			let y = row * cellSize;
 			// رسم حدود المربع
 			ctx.strokeStyle = '#000';
 			ctx.strokeRect(x, y, cellSize, cellSize);
-    	}
+		}
 	}
 }
+//----------------------------------------------------------------------------------------------------------
+function close() {
+	if (confirm("Are you sure about that?"))
+	{
+		window.close();
+	}
+}
+//----------------------------------------------------------------------------------------------------------
+document.addEventListener("DOMContentLoaded", function() 
+{
+	let showName = document.getElementById("show-name"); 
+	let playerName = localStorage.getItem("playerName"); // استرجاع الاسم
+	showName.textContent = `Welcome ${playerName}` ; // عرض الاسم وطباعته 
+	game(); 
+});
